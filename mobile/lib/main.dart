ebe4e8e91d02dd73204d97d3764f9e53783b1cd9
Kamil Mrowiec <kamil.mrowiec@iteo.com>
2021-11-24 22:12:30 +0100

    Working on analyzing

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -62,6 +62,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    SizedBox();
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
